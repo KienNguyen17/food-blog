00d6f9418a2d33cfe29b5b6338b2b872391b2fe8
Kien Nguyen <knguyen2@macalester.edu>
2025-07-28 01:07:00 -0500

    for vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,5 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
-const nextConfig = {
-    images: {
-        unoptimized: true, // Disable default image optimization
-    },
-    assetPrefix: isProd ? '/food-blog/' : '',
-    basePath: isProd ? '/food-blog' : '',
-    output: "export",  // <=== enables static exports
-    reactStrictMode: true,
-};
+
+const nextConfig = {};
 
 export default nextConfig;
