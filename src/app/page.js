9d8fd31e17eea98d3817903450c430bc171d0f19
Kien Nguyen <knguyen2@macalester.edu>
2025-07-28 21:21:58 -0500

    fix styling w Hieu's feedback

--- src/app/page.js
+++ src/app/page.js
@@ -27,11 +27,11 @@ export default function Home() {
             return (
                 <div 
                     key={`${item.name} - ${item.location}`}
-                    className='mt-4 shadow-[#764838] shadow-md p-3 w-60'
+                    className='mt-4 shadow-[#764838] shadow-md p-3 w-70 h-50'
                 >
-                    <p className='text-2xl text-extrabold font-serif text-[#764838]'>{item.name}</p>
-                    <p className=''>{item.location}</p>
-                    <p className='text-gray-500 italic text-extralight'>{item.note}</p>
+                    <p className='text-2xl font-serif text-[#764838]'>{item.name}</p>
+                    <p className='font-normal'>{item.location}</p>
+                    <p className='text-gray-500 italic font-extralight'>{item.note}</p>
                 </div>
             )
         })
@@ -43,10 +43,10 @@ export default function Home() {
             return (
                 <div 
                     key={item[0]}
-                    className={`h-10 text-center rounded-2xl mr-2 p-3 cursor-pointer shadow-[#764838] shadow-sm text-xs font-normal 
-                        hover:bg-[#FACA78] hover:text-[#68c7c1] 
-                        ${currentDish == item[0] ? 'bg-[#FACA78]' : 'bg-[#DD5341]'}
-                        ${currentDish == item[0] ? 'text-[#68c7c1] ' : 'text-[#ffffff]'}`}
+                    className={`h-10 text-center rounded-2xl mr-2 p-3 cursor-pointer shadow-[#9f2f20] shadow-sm text-xs font-normal 
+                        hover:bg-[#FACA78] hover:text-[#9f2f20] 
+                        ${currentDish == item[0] ? 'bg-[#FACA78]' : 'bg-[#9f2f20]'}
+                        ${currentDish == item[0] ? 'text-[#9f2f20] ' : 'text-[#FACA78]'}`}
                     onClick={() => setCurrectDish(item[0])}
                 >
                     {item[0]}
@@ -60,10 +60,10 @@ export default function Home() {
             return (
                 <div 
                     key={item[0]}
-                    className={`sm:w-50 md:w-50 lg:w-50 p-3 bg-[#DD5341] text-sm mb-2 cursor-pointer shadow-[#764838] shadow-md
-                        hover:bg-[#FACA78] hover:text-[#68c7c1] 
-                        ${currentCategory == item[0] ? 'bg-[#FACA78]' : 'bg-[#DD5341]'}
-                        ${currentCategory == item[0] ? 'text-[#68c7c1] ' : 'text-[#ffffff]'}`}
+                    className={`font-elsie sm:w-50 md:w-50 lg:w-50 p-3 bg-[#9f2f20] text-base mb-3 cursor-pointer shadow-[#764838] shadow-md
+                        hover:bg-[#FACA78] hover:text-[#9f2f20] 
+                        ${currentCategory == item[0] ? 'bg-[#FACA78]' : 'bg-[#9f2f20]'}
+                        ${currentCategory == item[0] ? 'text-[#9f2f20] ' : 'text-[#FACA78]'}`}
                     onClick={() => {
                         setCurrentCategory(item[0])
                         setCurrectDish(Object.keys(item[1])[0])
@@ -77,16 +77,16 @@ export default function Home() {
 
     return (
         <div className=''>
-            <div className='flex justify-start flex-wrap pb-8 pt-12 pl-8 pr-8 sm:pl-24 md:pl-24 lg:pl-24 sm:pr-24 md:pr-24 lg:pr-24 gap-10 bg-[#DD5341] w-screen sm:w-screen md:w-screen lg:w-screen h-20" border-[#764838] border-b-12'>
+            <div className='flex justify-start flex-wrap pb-8 pt-12 pl-8 pr-8 sm:pl-24 md:pl-24 lg:pl-24 sm:pr-24 md:pr-24 lg:pr-24 gap-10 bg-[#9f2f20] w-screen sm:w-screen md:w-screen lg:w-screen h-20" border-[#764838] border-b-12'>
                 <div className='max-w-xl'>
                     <Image src="/icon.jpg" width={200} height={200} alt="website-logo" className='object-scale-down'/>
                 </div>
 
                 <div className='flex flex-col'>
-                    <h1 className='font-serif text-8xl text-[#FACA78]'>
+                    <h1 className='font-elsie font-bold text-8xl text-[#FACA78]'>
                         Anh Nguyen
                     </h1>
-                    <h1 className='font-serif text-8xl text-[#FACA78]'>
+                    <h1 className='font-elsie font-bold text-8xl text-[#FACA78]'>
                         Food Review
                     </h1>
                 </div>
@@ -107,7 +107,6 @@ export default function Home() {
                 <div className='flex-col'>
                     <div className='flex flex-wrap gap-y-2'>
                         {renderDishes(currentCategory)} 
-
                     </div>
                     <div className='flex flex-wrap justify-start gap-x-10'>
                         {renderEntries(currentCategory, currentDish)}
